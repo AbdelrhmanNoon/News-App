beaf69aeb38c52789d3d3c4cf11c4e15a4c9846a
AbdelrhmanNoon <abdelrhmanameennoon@gmail.com>
2022-11-01 14:27:45 +0200

    Add secret key

---
+++ NewsApp/NewsApp/Support/SecretKey/SecretKey.swift
@@ -0,0 +1,14 @@
+//
+//  SecretKey.swift
+//  NewsApp
+//
+//  Created by ABDO on 30/10/2022.
+//
+
+import Foundation
+
+// MARK: - Important Notes:
+// Create your Own key to test the application
+// When we work on real app we should store secret keys in keyChain.
+
+let apiKey: String = "5aca5629bb614f1c91625242940d9e28"
